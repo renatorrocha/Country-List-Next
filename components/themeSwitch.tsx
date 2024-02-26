e8331151fca9d2758594c1ba6b4a2d0420528640
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-02-26 17:43:54 -0300

    feat (Navbar): theme Switch

--- components/themeSwitch.tsx
+++ components/themeSwitch.tsx
@@ -4,15 +4,37 @@ import { useTheme } from "next-themes";
 import { Switch } from "./ui/switch";
 import { Label } from "./ui/label";
 import { useState } from "react";
+import { Button } from "./ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function ThemeSwitch() {
-  const [toggleTheme, setToggleTheme] = useState("light" || "dark");
   const { setTheme } = useTheme();
 
   return (
-    <section className="flex items-center gap-2">
-      <Switch id="dark-mode-switch" onClick={() => setTheme("light")} />
-      <Label htmlFor="dark-mode-switch">Dark Mode</Label>
-    </section>
+    <DropdownMenu>
+      <DropdownMenuTrigger className="border border-zinc-100 dark:border-zinc-800 px-2 py-1 rounded-sm hover:bg-zinc-200 dark:hover:bg-zinc-800 transition-all duration-300">
+        Theme Switch
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
