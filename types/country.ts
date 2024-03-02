d2199ff7f1814ec0e037d0cfa919062cb44f8548
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-03-02 17:14:27 -0300

    fix: country types

--- types/country.ts
+++ types/country.ts
@@ -1,10 +1,31 @@
 export interface ICountry {
-  name: { common: string };
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      language: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+
+  population: number;
+  region: string;
+  capital: string;
+  subregion: string;
+  tld: string;
+  languages: string[];
+
+  currencies: {
+    currency: {
+      name: string;
+      symbol: string;
+    };
+  };
+
   flags: {
     svg: string;
     alt: string;
   };
-  population: number;
-  region: string;
-  capital: string;
 }
