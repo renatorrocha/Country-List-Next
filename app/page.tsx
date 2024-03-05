280ef242c44620c6bb0220522d2b2e2994e3adda
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-03-05 17:37:35 -0300

    remove:  searchBar

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import SearchBar from "@/components/searchbar";
 export default async function Home() {
   return (
     <main className="mx-auto max-h-screen max-w-[1440px] px-6 pt-10 md:px-24">
-      <SearchBar />
+      {/* <SearchBar /> */}
       <ListAllCountries />
     </main>
   );
